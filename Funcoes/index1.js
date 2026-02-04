ed30464050029cf53aac0fe5beffce8d33cfbdcf
Kauanzera <kauan.nana9@gmail.com>
2026-02-03 22:06:49 -0300

    Funções em Js

---
+++ Funcoes/index1.js
@@ -0,0 +1,29 @@
+// Funções Individuais
+function colocarTorradeira(){
+    console.log("Colocando o pão na torradeira...");
+}
+
+function paoTorrado(){
+    console.log("O pão está torrado!\n");
+}
+
+function torrar(){
+    console.log("Torrando o pão...");
+}
+
+// Função Principal
+function executar(){
+    colocarTorradeira();
+    torrar();
+    paoTorrado();
+}
+
+function seila(){
+    console.log("OLHA A COBRAAAAAA");
+}
+
+executar();
+
+seila();
+seila();
+seila();
